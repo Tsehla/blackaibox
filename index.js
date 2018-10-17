e05461c71fc5d951a46c25fd9dc9cca23a28959a
Tsehla Nkhi <petrusnk@gmail.com>
2018-10-17 14:12:14 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,177 @@
+/* +++++++++++++++++++++++++++++++++++++++
+==> on init
+/+++++++++++++++++++++++++++++++++++++++ */
+//--> built awarnes//first eyes open
+/* ========================================
+ makes calls, get deifinition of being constantly using default memeory.
+ should have way to get modified memory per result of [ recives call back and gives definition of being constantly ]
+/======================================== */
+// starts with [a  spark ] 
+var life_line_spark = setInterval(function(){ /*alert()*/}, 100);
+
+
+/*=======================================
+recives call back and gives definition of being constantly
+{ should confirm results from called components, and update [ acquired memory//bits and pices contribuitng to what makes being unique ], other abilities will refer to [ acquired memory//bits and pices contribuitng to what makes being unique ] contents   }
+/======================================= */
+//feel this function does mislead for a kid, kid know not awareness, but do know self diffrently, hence in hands of mother does not cry, but cries when away, as new born, self is known not as his/her body but combined with mother
+function being_definition_status_giver(identifier, status){
+    
+    
+    
+}
+
+
+
+
+
+/* +++++++++++++++++++++++++++++++++++++++
+default memory//what gives definition to the being
+/+++++++++++++++++++++++++++++++++++++++ */
+/* =====================================
+-> looking at a born baby, have componets but no idea they exist or use except some expressed involuntary
+-> will take assumption  of perfectly functional new born
+========================================*/
+var being_unknown_expressed_components ={
+    //kid experience and express result//here used component for easyfying
+    neck : 1,//head movement
+    eyes : 1, //eyseight
+    mouth : 1, //voice
+    ears : 1, //hearing
+    hand : 1, //grabing
+    nose : 1, //scent
+    legs : 1, //kicking
+    skin : 1,//touch/temperature sensor
+}
+
+
+
+/* +++++++++++++++++++++++++++++++++++++++
+-> acquired memory//bits and pices contribuitng to what makes being unique
+-> self acknowledgment memory
+-> semi permanent can be altered
++++++++++++++++++++++++++++++++++++++++ */
+/*
+-> each body componets keeps its own data, or memory
+-> this data or memory has no relationship with data or memeory kept by onother component,
+-> 
+
++++++++++++++++++++++++++++++++++++++++ */
+
+var acquired_memory = {
+    
+    neck : {},
+    eyes : {},
+    mouth : {},
+    ears : {},
+    hand : {},
+    nose : {},
+    legs : {},
+    skin : {},
+    
+    
+    //purposed mememory//if memeory has enough data for clear re-call of the moment
+    
+    
+    //unclassified memory//if memeory data is insuffient for re-recodnition to the latter
+}
+
+
+
+/* +++++++++++++++++++++++++++++++++++++++
+working memeory//memory that links to current and old to attemp to make sense
+-> is available so long as being is consitrated on, and discarded when focus lost after some time
+-> can be tranfered to semi permant if being find it valuable, or come across same stimuli multiple times
+ +++++++++++++++++++++++++++++++++++++++ */
+
+
+
+
+
+/*+++++++++++++++++++++++++++++++++++++++
+ now memeory//the now moment//now stimuli
+ +++++++++++++++++++++++++++++++++++++++ */
+
+
+
+
+
+/*+++++++++++++++++++++++++++++++++++++++
+==> action : result of [ working memeory//memory that links to current and old to attemp to make sense ] and [  now memeory//the now moment//now stimuli ]
+
++++++++++++++++++++++++++++++++++++++++ */
+
+
+/*+++++++++++++++++++++++++++++++++++++++
+========================================
+-->actual components, hardware
+--> give result of functionality
+========================================
++++++++++++++++++++++++++++++++++++++++*/
+
+/* ========================================
+ head
+ //as a kid new born
+========================================*/
+
+//-> hair
+
+
+//-> eyes
+function component_eyes(){
+    // sight is true
+    return being_definition_status_giver('eyesight', 1);
+    
+}
+
+//-> mouth
+function component_mouth(){
+    // screem is true
+    return being_definition_status_giver('voice', 1);
+    
+}
+
+//-> nose
+function component_nose(){
+    // scent is true
+    return being_definition_status_giver('smell', 1);
+    
+}
+
+//-> ears
+function component_ears(){
+    // sound is true
+    return being_definition_status_giver('sound', 1);
+    
+}
+
+//-> neck
+function component_neck(){
+    // tilt is true
+    return being_definition_status_giver('tilt', 1);
+    
+}
+
+//->chest
+
+
+//-> torso
+
+
+
+//-> legs
+
+function component_legs(){
+    // kicking is true
+    return being_definition_status_giver('kicking', 1);
+    
+}
+
+
+
+
+
+
+
+
+
